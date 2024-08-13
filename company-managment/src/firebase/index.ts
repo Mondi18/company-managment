f7cb53fcf4fe28b43b42dc316f2d5b48423b7aa7
Ati <nagyonkellvalamihez@gmail.com>
2024-08-13 17:03:55 +0200

    error handling

--- company-managment/src/firebase/index.ts
+++ company-managment/src/firebase/index.ts
@@ -1,15 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./config";
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth';
-import { getDatabase, ref, get, query, orderByChild, equalTo, set, onValue, update } from "firebase/database";
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from "firebase/database";
 
 import {
-    doc,
-    getDoc,
     getFirestore,
 } from 'firebase/firestore';
-import { Order } from "../orders/types";
-import { Driver } from "../drivers/types";
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
