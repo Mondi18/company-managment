fa05e9060885f28048628615c86ede4a2e85bc9e
Ati <nagyonkellvalamihez@gmail.com>
2024-08-13 16:02:19 +0200

    adding some style to Navbar

--- company-managment/src/components/Navbar.tsx
+++ company-managment/src/components/Navbar.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { create, props } from '@stylexjs/stylex'
+
+const nav = create({
+    base: {
+        textAlign: 'center',
+        color: 'red',
+        fontSize: '22px'
+    },
+    highlighted: {
+        color: 'rebeccapurple',
+    },
+});
 
 const Navbar = () => {
+
     return (
-        <div>
+        <div {...props(nav.base, nav.highlighted)}>
             Navbar
         </div>
     )
