4f0fdf36daab0c6f47d0593f635b3378b023da75
Ati <nagyonkellvalamihez@gmail.com>
2024-08-14 14:53:34 +0200

    add updateEmployees and addEmployees method + for example

--- company-managment/src/firebase/index.ts
+++ company-managment/src/firebase/index.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./config";
 import { getAuth } from 'firebase/auth';
-import { get, getDatabase, ref } from "firebase/database";
+import { get, getDatabase, ref, set, push, update } from "firebase/database";
 
 import {
     getFirestore,
 } from 'firebase/firestore';
-import { Employee } from '../data/type';
+import { Employee, JobPosition, Level } from '../data/type';
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
@@ -20,3 +20,38 @@ export const listEmployees = async (): Promise<Record<string, Employee> | undefi
     if (!snapshot.exists()) return undefined;
     return snapshot.val();
 }
+
+export const addEmployees = async (employee: Employee): Promise<void> => {
+    const EmployeesRef = ref(realtime, '/work/employee');
+    const newEmployeeRef = push(EmployeesRef);
+    await set(newEmployeeRef, employee);
+}
+
+export const updateEmployees = async (employeeId: string, partialEmployee: Partial<Employee>): Promise<void> => {
+    const employeeRef = ref(realtime, `/work/employee/${employeeId}`);
+    await update(employeeRef, partialEmployee);
+}
+const newEmployee: Employee = {
+    firstName: "András",
+    lastName: "Tóth",
+    email: "andras.toth@example.com",
+    level: Level.Senior,
+    jobPosition: JobPosition.SystemAdministrator,
+    salary: 100,
+    isBusy: false
+}
+
+addEmployees(newEmployee)
+    .then(() => console.log("Employee added successfully!"))
+    .catch((error) => console.error("Error adding employee :", error))
+
+
+const Id = "-O4FuQhgHk5tOAIHhXuB";
+const updatedEmployeeData: Partial<Employee> = {
+    salary: 1000,
+    isBusy: true
+}
+
+updateEmployees(Id, updatedEmployeeData)
+    .then(() => console.log("Employee updated successfully!"))
+    .catch((error) => console.error("Error updating employee: ", error));
