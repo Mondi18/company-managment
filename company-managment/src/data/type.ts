fa96423449c5b8408d9c3a2e5ce54df86aed2775
Mondi18 <vargamondi@gmail.com>
2024-08-14 14:14:23 +0200

    update Employee type

--- company-managment/src/data/type.ts
+++ company-managment/src/data/type.ts
@@ -1,12 +1,10 @@
 export type Employee = {
-    id: number,
     lastName: string,
     firstName: string,
     email: string
     level: Level;
     jobPosition: JobPosition;
     salary: number
-
 }
 export enum Level {
     Junior,
