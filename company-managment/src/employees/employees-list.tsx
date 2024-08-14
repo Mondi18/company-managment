2aa60c814bd560d6b84a880d0634640d4002f558
Ati <nagyonkellvalamihez@gmail.com>
2024-08-14 17:08:19 +0200

    add delete method the employees with buttons

--- company-managment/src/employees/employees-list.tsx
+++ company-managment/src/employees/employees-list.tsx
@@ -4,11 +4,13 @@ import { listEmployees } from '../firebase';
 import { Employee } from '../data/type';
 import { create, props } from '@stylexjs/stylex';
 import { useNavigate } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import { deleteEmployee } from '../firebase/index.ts';
 
 const TableStyle = create({
-  
+
   headerCell: {
-    width: '14.2857%', 
+    width: '14.2857%',
     textAlign: 'center',
     border: '1px solid #ddd',
     padding: '8px',
@@ -18,17 +20,21 @@ const TableStyle = create({
     border: '1px solid #ddd',
     padding: '8px',
   },
-  tr:{
-    cursor:"pointer"
+  tr: {
+    cursor: "pointer"
   },
-  bg:{
-    backgroundColor:'bisque'
+  bg: {
+    backgroundColor: 'bisque'
+  },
+  button: {
+    margin: '0.5rem',
+    color: 'red'
   }
- 
+
 });
 
 const EmployeesList = () => {
-    const navigate=useNavigate();
+  const navigate = useNavigate();
   const [employees, setEmployees] = useState<Record<string, Employee> | undefined>(undefined);
 
   useEffect(() => {
@@ -40,7 +46,26 @@ const EmployeesList = () => {
       console.error("Error when fetching employees:", error);
     });
   }, []);
-  
+
+  const handleDelete = async (id: string) => {
+    try {
+      await deleteEmployee(id);
+
+      setEmployees(prevEmployees => {
+        if (prevEmployees) {
+          const updatedEmployees = { ...prevEmployees };
+          delete updatedEmployees[id];
+          return updatedEmployees;
+
+        }
+        return prevEmployees;
+      });
+      console.log('Employee deleted successfully!');
+    } catch (error) {
+      console.error('Error deleting employee:', error)
+    }
+  };
+
 
   return (
 
@@ -55,11 +80,12 @@ const EmployeesList = () => {
           <th {...props(TableStyle.headerCell)}>Job Position</th>
           <th {...props(TableStyle.headerCell)}>Level</th>
           <th {...props(TableStyle.headerCell)}>Salary</th>
+          <th {...props(TableStyle.headerCell)}>Actions</th>
         </tr>
       </thead>
       <tbody>
         {employees && Object.entries(employees).map(([id, employee]) => (
-          <tr key={id} onClick={()=>navigate("/employees-edit")} {...props(TableStyle.tr)}>
+          <tr key={id} onClick={() => navigate("/employees-edit")} {...props(TableStyle.tr)}>
             <td {...props(TableStyle.tableCell)}>{employee.email}</td>
             <td {...props(TableStyle.tableCell)}>{employee.lastName}</td>
             <td {...props(TableStyle.tableCell)}>{employee.firstName}</td>
@@ -67,6 +93,19 @@ const EmployeesList = () => {
             <td {...props(TableStyle.tableCell)}>{employee.jobPosition}</td>
             <td {...props(TableStyle.tableCell)}>{employee.level}</td>
             <td {...props(TableStyle.tableCell)}>{employee.salary}</td>
+            <td {...props(TableStyle.tableCell)}>
+              <Button
+                variant="contained"
+                color="error"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleDelete(id);
+                }}
+                {...props(TableStyle.button)}
+              >
+                Delete
+              </Button>
+            </td>
             <td></td>
           </tr>
         ))}
