90f3b1db3d74e7b68c24933e2b34cfa57f3867db
Mondi18 <vargamondi@gmail.com>
2024-08-14 15:08:24 +0200

    Delete unused testData

--- company-managment/src/firebase/index.ts
+++ company-managment/src/firebase/index.ts
@@ -6,7 +6,7 @@ import { get, getDatabase, ref, set, push, update } from "firebase/database";
 import {
     getFirestore,
 } from 'firebase/firestore';
-import { Employee, JobPosition, Level } from '../data/type';
+import { Employee } from "../data/type";
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
@@ -31,27 +31,3 @@ export const updateEmployees = async (employeeId: string, partialEmployee: Parti
     const employeeRef = ref(realtime, `/work/employee/${employeeId}`);
     await update(employeeRef, partialEmployee);
 }
-const newEmployee: Employee = {
-    firstName: "András",
-    lastName: "Tóth",
-    email: "andras.toth@example.com",
-    level: Level.Senior,
-    jobPosition: JobPosition.SystemAdministrator,
-    salary: 100,
-    isBusy: false
-}
-
-addEmployees(newEmployee)
-    .then(() => console.log("Employee added successfully!"))
-    .catch((error) => console.error("Error adding employee :", error))
-
-
-const Id = "-O4FuQhgHk5tOAIHhXuB";
-const updatedEmployeeData: Partial<Employee> = {
-    salary: 1000,
-    isBusy: true
-}
-
-updateEmployees(Id, updatedEmployeeData)
-    .then(() => console.log("Employee updated successfully!"))
-    .catch((error) => console.error("Error updating employee: ", error));
