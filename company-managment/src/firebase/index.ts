62df4f2499a29946aaae3ef5bd56eba926dee2a7
Ati <nagyonkellvalamihez@gmail.com>
2024-08-15 12:11:01 +0200

    adding Crud Operations and example

--- company-managment/src/firebase/index.ts
+++ company-managment/src/firebase/index.ts
@@ -4,15 +4,26 @@ import { getAuth } from 'firebase/auth';
 import { get, getDatabase, ref, set, push, update, remove } from "firebase/database";
 
 import {
+    collection,
+    addDoc,
+    doc,
+    getDocs,
     getFirestore,
+    getDoc,
+    updateDoc,
+    deleteDoc,
 } from 'firebase/firestore';
-import { Employee } from "../data/type";
+import { Employee, Order, WebStatus, Web, WebStyle } from "../data/type";
+
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const realtime = getDatabase(app);
+
+//employee CRUD operations
 
 export const listEmployees = async (): Promise<Record<string, Employee> | undefined> => {
     const EmployeesRef = ref(realtime, '/work/employee');
@@ -36,3 +47,67 @@ export const deleteEmployee = async (employeeId: string): Promise<void> => {
     const EmployeeRef = ref(realtime, `/work/employee/${employeeId}`);
     await remove(EmployeeRef);
 }
+
+//Order CRUD operations
+
+export const addOrder = async (order: Order): Promise<Order> => {
+    const OrderRef = collection(db, 'order');
+    await addDoc(OrderRef, order);
+    return order;
+}
+
+export const listOrders = async (): Promise<Record<string, Order> | undefined> => {
+    const OrderRef = collection(db, 'order');
+    const snapshot = await getDocs(OrderRef);
+    if (snapshot.empty) {
+        return undefined;
+    }
+
+    const orders: Record<string, Order> = {};
+    snapshot.forEach(doc => {
+        orders[doc.id] = doc.data() as Order;
+    })
+    return orders;
+}
+
+export const getOrderById = async (orderId: string): Promise<Order | undefined> => {
+    const OrderRef = doc(db, 'order', orderId);
+    const snapshot = await getDoc(OrderRef);
+    if (!snapshot.exists()) return undefined;
+    return snapshot.data() as Order;
+}
+
+export const updateOrder = async (orderId: string, partialOrder: Partial<Order>): Promise<void> => {
+    const OrderRef = doc(db, 'order', orderId);
+    try {
+        await updateDoc(OrderRef, partialOrder);
+    } catch (error) {
+        console.error("Error deleting document :", error)
+    }
+}
+
+export const deleteOrder = async (orderId: string): Promise<void> => {
+    const OrderRef = doc(db, 'order', orderId);
+    try {
+        await deleteDoc(OrderRef);
+    } catch (error) {
+        console.error("Error deleting document :", error)
+    }
+}
+
+/*const newOrder: Order = {
+    goal: Web.Blog,
+    pages: 5,
+    style: WebStyle.Minimalist,
+    service: true,
+    deadline: new Date(),
+    notice: [],
+    status: WebStatus.Processing,
+    price: 1000
+};
+addOrder(newOrder);
+const orders = listOrders();
+console.log(orders);
+getOrderById('cdpA55ZRj2DQ3X888gJS').then(order => console.log(order));
+updateOrder('cdpA55ZRj2DQ3X888gJS', { status: WebStatus.InProgress });
+deleteOrder('cdpA55ZRj2DQ3X888gJS');*/
