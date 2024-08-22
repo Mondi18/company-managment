f7a944234decc6358ef90fe707acb57a02a48360
Ati <nagyonkellvalamihez@gmail.com>
2024-08-22 14:04:21 +0200

    add Login pages with functions

--- company-managment/src/components/Login.tsx
+++ company-managment/src/components/Login.tsx
@@ -1,10 +1,81 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { loginUser, logoutUser, signInWithGoogle, onAuthStateChangeListener } from '../firebase';
+import { User } from 'firebase/auth';
+
 
 
 const Login = () => {
-    return (
-        <div>Login</div>
-    )
-}
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [user, setUser] = useState<User | null>(null);
 
-export default Login
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangeListener((currentUser) => {
+            setUser(currentUser);
+        });
+
+        return () => unsubscribe();
+    }, []);
+    const handleLogin = async () => {
+        try {
+            await loginUser(email, password);
+            console.log('Login successful!');
+        } catch (error) {
+            console.log('Login failed. Check the console for details.');
+            console.error('Login error:', error);
+        }
+    };
+    const handleLogout = async () => {
+        try {
+            await logoutUser();
+            console.log("Log out is successfully!");
+
+        } catch (error) {
+            console.log('Logout failed. Check the console for details.');
+            console.error('Logout error:', error);
+        }
+    }
+
+    const handleGoogleSignIn = async () => {
+        try {
+            await signInWithGoogle();
+            console.log("nagyon mennőő")
+            console.log('Login with Google successful!');
+        } catch (error) {
+            console.log('Login with Google failed. Check the console for details.');
+            console.error('Google Sign-In error:', error);
+        }
+    };
+
+    return (
+        <div>
+            <h2>Login</h2>
+            {!user ? (
+                <>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button onClick={handleLogin}>Login</button>
+                    <hr />
+                    <button onClick={handleGoogleSignIn}>Login with Google</button>
+                </>
+            ) : (
+                <>
+                    <p>Welcome, {user.email}</p>
+                    <button onClick={handleLogout}>Logout</button>
+                </>
+            )}
+        </div>
+    );
+};
+
+export default Login;
