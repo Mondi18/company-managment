02aa391c7ff4c6ac30c4ca06b125a27b80720564
Mondi18 <vargamondi@gmail.com>
2024-08-27 15:30:15 +0200

    Add Navbar, css

--- company-managment/src/routes/Root.tsx
+++ company-managment/src/routes/Root.tsx
@@ -1,16 +1,23 @@
-
+import { create, props } from '@stylexjs/stylex';
 import { Outlet } from 'react-router-dom';
+import { AuthProvider } from '../auth/auth-provider';
 import Navbar from '../components/Navbar';
 
-
+const ROOT_STYLES = create({
+    container: {
+        display: 'grid',
+        gridTemplateRows: 'auto 1fr',
+        minHeight: '100vh',
+    },
+});
 
 const Root: React.FC = () => (
-
-    <div>
-        <Navbar />
-        <Outlet />
-    </div>
-
+    <AuthProvider>
+        <div {...props(ROOT_STYLES.container)}>
+            <Navbar />
+            <Outlet />
+        </div>
+    </AuthProvider>
 );
 
 export default Root;
