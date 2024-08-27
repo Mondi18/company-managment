2962c05678a95c344cbdcb50c466b84f63fe3df1
Ati <nagyonkellvalamihez@gmail.com>
2024-08-27 16:23:43 +0200

    add cursor-pointer

--- company-managment/src/components/Login.tsx
+++ company-managment/src/components/Login.tsx
@@ -2,44 +2,45 @@ import { create, props } from '@stylexjs/stylex';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../auth/use-auth';
 const LOGIN_STYLES = create({
-  container: {
-    display: 'flex',
-    width: '100%',
-    height: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: '2rem',
-  },
-  button: {
-    display: 'flex',
-    gap: '.5rem',
-    alignItems: 'center',
-    fontSize: '2rem',
-    textTransform: 'uppercase',
-  },
+    container: {
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '2rem',
+    },
+    button: {
+        display: 'flex',
+        gap: '.5rem',
+        alignItems: 'center',
+        fontSize: '2rem',
+        textTransform: 'uppercase',
+        cursor: 'pointer'
+    },
 });
 
 const Login: React.FC = () => {
-  const navigate = useNavigate();
-  const { loginWithGoogle } = useAuth();
+    const navigate = useNavigate();
+    const { loginWithGoogle } = useAuth();
 
-  const onLogin = () => {
-    loginWithGoogle().then(() => {
-      navigate('/employees-edit');
-    });
-  };
+    const onLogin = () => {
+        loginWithGoogle().then(() => {
+            navigate('/employees-edit');
+        });
+    };
 
-  return (
-    <main {...props(LOGIN_STYLES.container)}>
-      <button onClick={onLogin}>
-        <div {...props(LOGIN_STYLES.button)}>
-          Login with Google
-       <img src='../public/google.png'/>
-        </div>
+    return (
+        <main {...props(LOGIN_STYLES.container)}>
+            <button onClick={onLogin}>
+                <div {...props(LOGIN_STYLES.button)}>
+                    Login with Google
+                    <img src='../public/google.png' />
+                </div>
 
-      </button>
-    </main>
-  );
+            </button>
+        </main>
+    );
 };
 
 export default Login;
