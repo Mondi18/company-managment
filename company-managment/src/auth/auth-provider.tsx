010ff95e31c2e489b9c595a7b0bf921c8cafa49f
Ati <nagyonkellvalamihez@gmail.com>
2024-09-06 09:34:33 +0200

    refactroing method with role, searching a database

--- company-managment/src/auth/auth-provider.tsx
+++ company-managment/src/auth/auth-provider.tsx
@@ -3,27 +3,43 @@ import { useState, useEffect, useMemo, PropsWithChildren } from 'react';
 import { auth, loginPopup, logout } from '../firebase';
 import { authContext } from './auth-context';
 import { AuthContext } from './types';
+import { CustomerUser, UserRole } from '../data/type';
+import { getDoc, doc } from 'firebase/firestore';
+import { db } from '../firebase';
 
 export const AuthProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(true);
+  const [role, setRole] = useState<UserRole | null>(null);
 
-  useEffect(
-    () =>
-      onAuthStateChanged(auth, async (newUser) => {
-        setUser(newUser);
-        setLoading(false);
-      }),
-    []
-  );
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (newUser) => {
+      if (newUser) {
+        const userDoc = await getDoc(doc(db, 'users', newUser.uid));
 
+        if (userDoc.exists()) {
+          const userData = userDoc.data() as CustomerUser;
+          setUser(newUser);
+          setRole(userData.role);
+        }
+      } else {
+        setUser(null);
+        setRole(null);
+      }
+      setLoading(false);
+      console.log("Auth state changed:", newUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
   const value = useMemo<AuthContext>(
     () => ({
       user,
+      role,
       loginWithGoogle: loginPopup,
       logout,
     }),
-    [user]
+    [user, role]
   );
   return <authContext.Provider value={value}> {!loading && children}</authContext.Provider>;
 };
