b6738f7918e560bec3e111a99cc37a063cbcdc2f
Ati <nagyonkellvalamihez@gmail.com>
2024-09-06 09:35:41 +0200

    add role

--- company-managment/src/auth/types.ts
+++ company-managment/src/auth/types.ts
@@ -1,7 +1,9 @@
 import { User } from 'firebase/auth';
+import { UserRole } from '../data/type';
 
 export type AuthContext = {
   user: User | null;
+  role: UserRole | null;
   loginWithGoogle: () => Promise<void>;
   logout: () => Promise<void>;
 };
